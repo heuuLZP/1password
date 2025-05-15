3cdefe2d2c780058078bb8b3641ddc5e9cddb7d1
heuuLZP <heuuLZP@163.com>
2025-05-15 09:51:14 +0800

    feat: 新增 @vercel/analytics 依赖并在主应用中集成分析组件

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react"
 import App from './App';
 import StyleRegistry from './components/StyleRegistry';
 import 'antd/dist/antd.variable.css';
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StyleRegistry>
       <App />
+      <Analytics debug={true} />
     </StyleRegistry>
   </React.StrictMode>,
 ); 
