da0226302c661e7d66f9d0781dd3898c825215a0
heuuLZP <heuuLZP@163.com>
2025-08-25 18:27:12 +0800

    feat: 移除@vercel/analytics依赖及相关代码

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from "@vercel/analytics/react"
 import App from './App';
 import StyleRegistry from './components/StyleRegistry';
 import 'antd/dist/antd.variable.css';
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StyleRegistry>
       <App />
-      <Analytics debug={true} />
     </StyleRegistry>
   </React.StrictMode>,
 ); 
